dd4d0507ffdbe96769ce337b1ff7fcf5f8447c8c
Luis231968 <202000770@uthh.edu.mx>
2024-07-20 22:20:32 -0600

    lado usuario terminado

--- src/app/user/profile/pages/comentarios/comentarios.component.ts
+++ src/app/user/profile/pages/comentarios/comentarios.component.ts
@@ -10,11 +10,15 @@ import { ComentarioS } from '../../interfaces/GetComentarios.interface';
 export class ComentariosComponent implements OnInit{
   constructor(private profileService:ProfileService) { }
   public comentarios:ComentarioS[] = [];
+  isLoader: boolean = true;
   ngOnInit(): void {
     const idUser = localStorage.getItem('token')
     if(idUser !== null){
       this.profileService.getComentarios(idUser).subscribe(data => {
         this.comentarios = data.data;
+        setTimeout(() => {
+          this.isLoader = false;
+        }, 500);
       });
     }
   }
