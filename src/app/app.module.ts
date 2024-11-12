d19b98f7b262be02765db5ec42f2c540b860edc0
ElNao <naopubg28@gmail.com>
2024-11-12 11:54:05 -0600

    instalacion de dependencias para las graficas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,14 +23,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [
     MessageService,
-    ConfirmationService
+    ConfirmationService,
+    provideCharts(withDefaultRegisterables()),
   ],
   bootstrap: [AppComponent]
 })
